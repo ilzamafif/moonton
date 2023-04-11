dea93adf8093a14ebcdb176d0b69d4cf21a90723
Ilzam afif <ilzamafif27@gmail.com>
2023-04-11 14:18:23 +0700

    feat: Slicing Dashboard

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,6 +1,8 @@
 import Authenticated from "@/Layouts/Authenticated/Index";
 import Flickity from "react-flickity-component";
 import { Head } from "@inertiajs/react";
+import FeaturedMovie from "./FeaturedMovie";
+import MovieCard from "./MovieCard";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -29,50 +31,30 @@ export default function Dashboard() {
                     <Flickity className="gap-[30px]" options={flickityOptions}>
                         {[1, 2, 3, 4].map((i) => (
                             /* Movie Thumbnail  */
-                            <div
-                                className="absolute overflow-hidden group mr-[30px]"
+                            <FeaturedMovie
                                 key={i}
-                            >
-                                <img
-                                    src="/images/featured-1.png"
-                                    className="object-cover rounded-[30px] w-[520px] h-[340px]"
-                                    alt=""
-                                />
-                                {/* rating  */}
-                                <div className="rating absolute top-0 left-0">
-                                    <div className="p-[30px] flex items-center gap-1">
-                                        <img src="/icons/ic_star.svg" alt="" />
-                                        <span className="text-sm font-medium text-white mt-1">
-                                            4.5/5.0
-                                        </span>
-                                    </div>
-                                </div>
-                                {/* bottom detail */}
-                                <div
-                                    className="absolute bottom-0 h-[100px] left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
-                                rounded-br-[28px] flex justify-between items-center px-7 h-[130px]"
-                                >
-                                    <div>
-                                        <div className="font-medium text-[22px] text-white">
-                                            The Batman in Love
-                                        </div>
-                                        <p className="mb-0 text-white text-sm font-light">
-                                            Action • Horror
-                                        </p>
-                                    </div>
-                                    <div className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
-                                        <img
-                                            src="/icons/ic_play.svg"
-                                            width="50"
-                                            alt=""
-                                        />
-                                    </div>
-                                </div>
-                                <a
-                                    href="watching.html"
-                                    className="inset-0 absolute z-50"
-                                ></a>
-                            </div>
+                                slug="the-batman-in-love"
+                                name={`The Batman in Love ${i}`}
+                                category="Comedy"
+                                thumbnail="https://picsum.photos/id/1/300/300"
+                                rating={i + 1}
+                            />
+                        ))}
+                    </Flickity>
+                </div>
+                <div className="mt-[50px]">
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Browse
+                    </div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[1, 2, 3, 4, 5, 6].map((i) => (
+                            <MovieCard
+                                key={i}
+                                slug="the-batman-in-love"
+                                name={`The Batman in Love ${i}`}
+                                category="Comedy"
+                                thumbnail="https://picsum.photos/id/1/300/300"
+                            />
                         ))}
                     </Flickity>
                 </div>
