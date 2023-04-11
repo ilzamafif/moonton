dd2d55d5fa9944618f06d2db26f2ad8e17ce3f12
Ilzam afif <ilzamafif27@gmail.com>
2023-04-11 12:14:11 +0700

    feat: slicing sign in + custom component

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,6 +1,18 @@
 import { forwardRef, useEffect, useRef } from 'react';
-
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
+import PropTypes from "prop-types";
+const TextInput = forwardRef(function TextInput(
+    {
+        type = "text",
+        defaultValue,
+        variant = "primary",
+        placeholder,
+        isError,
+        className = "",
+        isFocused = false,
+        ...props
+    },
+    ref
+) {
     const input = ref ? ref : useRef();
 
     useEffect(() => {
@@ -15,11 +27,30 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
                 {...props}
                 type={type}
                 className={
-                    'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
-                    className
+                    `rounded-2xl bg-form-bg py-[13px] px-7 w-full ${
+                        isError && "input-error"
+                    } input${variant}` + className
                 }
                 ref={input}
+                placeholder={placeholder}
             />
         </div>
     );
 });
+
+TextInput.propTypes = {
+    type: PropTypes.oneOf(["text", "email", "password", "number", "file"]),
+    // name: PropTypes.string,
+    // value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    variant: PropTypes.oneOf(["primary", "error", "primary-outline"]),
+    // autoComplete: PropTypes.string,
+    // required: PropTypes.bool,
+    isFocused: PropTypes.bool,
+    // handleChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    isError: PropTypes.bool,
+};
+
+export default TextInput;
