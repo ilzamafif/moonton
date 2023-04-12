e4ce0038ff9f87ce30b744d7828a1b3d4d322cf1
Ilzam afif <ilzamafif27@gmail.com>
2023-04-12 19:55:23 +0700

    feta: middleware check user sebscription

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Carbon\Carbon;
 
 class User extends Authenticatable
 {
