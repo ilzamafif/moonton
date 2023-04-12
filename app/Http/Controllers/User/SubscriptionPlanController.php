e784b5c276dd1cfe8e597e06da2daad732fa3b81
Ilzam afif <ilzamafif27@gmail.com>
2023-04-12 09:55:38 +0700

    feat: Integraion Subscription plan on select plan

---
+++ app/Http/Controllers/User/SubscriptionPlanController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\SubscriptionPlan;
+use App\Models\UserSubscription;
+use Carbon\Carbon;
+use Auth;
+use Str;
+
+class SubscriptionPlanController extends Controller
+{
+    public function index()
+    {
+        $subscriptionPlans = SubscriptionPlan::all();
+
+        return inertia('User/Dashboard/SubscriptionPlan/Index', [
+            'subscriptionPlans' => SubscriptionPlan::all(),
+            'userSubscription' => null,
+        ]);
+    }
+
+    public function userSubscribe(Request $request, SubscriptionPlan $subscriptionPlan)
+    {
+        $data = [
+            'user_id' => Auth::id(),
+            'subscription_plan_id' => $subscriptionPlan->id,
+            'price' => $subscriptionPlan->price ,
+            'expired_date' => Carbon::now()->addMonths( $subscriptionPlan->active_period_in_months),
+            'payment_status' => 'success',
+        ];
+
+        $userSubscription = UserSubscription::create($data);
+        return redirect(route('user.dashboard.index'));
+    }
+}
+
--- resources/js/Layouts/Authenticated/Sidebar.jsx
+++ resources/js/Layouts/Authenticated/Sidebar.jsx
@@ -11,7 +11,10 @@ export default function Sidebar() {
                     <div className="links flex flex-col mt-[60px] h-full gap-[50px]">
                         <div>
                             <div className="text-gray-1 text-sm mb-4">Menu</div>
-                            <a href="/" className="side-link active">
+                            <a
+                                href={route("user.dashboard.index")}
+                                className="side-link active"
+                            >
                                 <svg
                                     width="24"
                                     height="24"
@@ -84,7 +87,9 @@ export default function Sidebar() {
                                 Others
                             </div>
                             <Link
-                                href={route("prototype.subscriptionPlan")}
+                                href={route(
+                                    "user.dashboard.subscriptionPlan.index"
+                                )}
                                 className="side-link"
                             >
                                 <svg
